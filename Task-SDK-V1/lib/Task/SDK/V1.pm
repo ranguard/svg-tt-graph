9ffbb12878fc1ca65dbba3cd836b28ae17db7302
ranguard <ranguard@5458bac8-4430-0410-bb98-dbc80393538f>
2009-11-29 11:32:23 +0000

    add Task::SDK::V1
    git-svn-id: http://llap.googlecode.com/svn/trunk@31 5458bac8-4430-0410-bb98-dbc80393538f

---
+++ Task-SDK-V1/lib/Task/SDK/V1.pm
@@ -0,0 +1,38 @@
+package Task::SDK::V1;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+
+$VERSION = '0.01';
+
+=head1 NAME
+
+Task::SDK::V1
+
+=head1 DESCRIPTION
+
+This will become the foundation for another project,
+please ignore for now.
+
+=head1 AUTHOR
+
+Leo Lapworth C<< <LLAP@cuckoo.org> >>
+
+=head1 SVN
+
+http://code.google.com/p/llap/
+
+=head1 COPYRIGHT
+
+Copyright (c) 2009, Leo Lapworth
+
+This module is free software; you can redistribute it or modify it
+under the same terms as Perl itself.
+
+=cut
+
+# http://www.cpantesters.org/distro/A/Acme-Buffy.json 
+
+1;
