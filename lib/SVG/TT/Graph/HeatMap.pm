6fa3be5215058d829b6b8d7ec9bea121089874bd
Stuart Skelton <stuartskelton@users.noreply.github.com>
2015-09-29 10:24:37 +0100

    Updated the documentation.
    The Pod has been overhauled to actually reflect what this module does.

--- lib/SVG/TT/Graph/HeatMap.pm
+++ lib/SVG/TT/Graph/HeatMap.pm
@@ -6,35 +6,46 @@ use Data::Dumper;
 use SVG::TT::Graph;
 use base qw(SVG::TT::Graph);
 
-our $VERSION = $SVG::TT::Graph::VERSION;
+our $VERSION     = $SVG::TT::Graph::VERSION;
 our $TEMPLATE_FH = \*DATA;
 
 
 =head1 NAME
 
-SVG::TT::Graph::XY - Create presentation quality SVG line graphs of XY data points easily
+SVG::TT::Graph::HeatMap - Create presentation quality SVG HeatMap graph of XYZ data points easily
 
 =head1 SYNOPSIS
 
-  use SVG::TT::Graph::XY;
+  use SVG::TT::Graph::HeatMap;
 
-  my @data_cpu  = (0.3, 23, 0.5, 54, 1.0, 67, 1.8, 12);
-  my @data_disk = (0.45, 12, 0.51, 26, 0.53, 23);
+  my @data_cpu = (
+                   {  x        => x_point1,
+                      y_point1 => 10,
+                      y_point2 => 200,
+                      y_point3 => 1000,
+                   },
+                   {  x        => x_point2,
+                      y_point1 => 100,
+                      y_point2 => 400,
+                      y_point3 => 500,
+                   },
+                   {  x        => x_point3,
+                      y_point1 => 1000,
+                      y_point2 => 600,
+                      y_point3 => 0,
+                   },
+                 );
 
-  my $graph = SVG::TT::Graph::XY->new({
-    'height' => '500',
-    'width'  => '300',
-  });
+  my $graph = SVG::TT::Graph::HeatMap->new(
+                                            { block_height => 24,
+                                              block_width  => 24,
+                                              gutter_width => 1,
+                                            } );
 
-  $graph->add_data({
-    'data'  => \@data_cpu,
-    'title' => 'CPU',
-  });
-
-  $graph->add_data({
-    'data'  => \@data_disk,
-    'title' => 'Disk',
-  });
+  $graph->add_data(
+                    { 'data'  => \@data_cpu,
+                      'title' => 'CPU',
+                    } );
 
   print "Content-type: image/svg+xml\n\n";
   print $graph->burn();
@@ -42,60 +53,26 @@ SVG::TT::Graph::XY - Create presentation quality SVG line graphs of XY data poin
 =head1 DESCRIPTION
 
 This object aims to allow you to easily create high quality
-SVG line graphs of XY data. You can either use the default style sheet
-or supply your own. Either way there are many options which can
-be configured to give you control over how the graph is
-generated - with or without a key, data elements at each point,
-title, subtitle etc.
+SVG HeatMap graphs of XYZ data. You can either use the default style sheet
+or supply your own. 
+
+Please note, the height and width of the final image is computed from the 
+size of the labels, block_height/block_with and gutter_size.
 
 =head1 METHODS
 
 =head2 new()
 
-  use SVG::TT::Graph::XY;
+  use SVG::TT::Graph::HeatMap;
 
-  my $graph = SVG::TT::Graph::XY->new({
+  my $graph = SVG::TT::Graph::HeatMap->new({
 
     # Optional - defaults shown
-    'height'              => 500,
-    'width'               => 300,
+    block_height => 24,
+    block_width  => 24,
+    gutter_width => 1,
 
-    'show_y_labels'       => 1,
-    'yscale_divisions'    => '',
-    'min_yscale_value'    => 0,
-    'max_yscale_value'    => '',
-
-    'show_x_labels'       => 1,
-    'xscale_divisions'    => '',
-    'min_xscale_value'    => '',
-    'max_xscale_value'    => '',
-    'stagger_x_labels'    => 0,
-    'rotate_x_labels'     => 0,
-    'y_label_formatter'   => sub { return @_ },
-    'x_label_formatter'   => sub { return @_ },
-
-    'show_data_points'    => 1,
-    'show_data_values'    => 1,
-    'rollover_values'     => 0,
-
-    'area_fill'           => 0,
-
-    'show_x_title'        => 0,
-    'x_title'             => 'X Field names',
-
-    'show_y_title'        => 0,
-    'y_title'             => 'Y Scale',
-
-    'show_graph_title'    => 0,
-    'graph_title'         => 'Graph Title',
-    'show_graph_subtitle' => 0,
-    'graph_subtitle'      => 'Graph Sub Title',
-    'key'                 => 0,
-    'key_position'        => 'right',
-
-    # Stylesheet defaults
-    'style_sheet'         => '/includes/graph.css', # internal stylesheet
-    'random_colors'       => 0,
+    'y_axis_order' => [],
   });
 
 The constructor takes a hash reference with values defaulted to those
@@ -104,11 +81,30 @@ to using the internal style sheet.
 
 =head2 add_data()
 
-  my @data_cpu  = (0.3, 23, 0.5, 54, 1.0, 67, 1.8, 12);
+  my @data_cpu = (
+                   {  x        => x_point1,
+                      y_point1 => 10,
+                      y_point2 => 200,
+                      y_point3 => 1000,
+                   },
+                   {  x        => x_point2,
+                      y_point1 => 100,
+                      y_point2 => 400,
+                      y_point3 => 500,
+                   },
+                   {  x        => x_point3,
+                      y_point1 => 1000,
+                      y_point2 => 600,
+                      y_point3 => 0,
+                   },
+                 );
   or
-  my @data_cpu = ([0.3,23], [0.5,54], [1.0,67], [1.8,12]);
-  or
-  my @data_cpu = ([0.3,23,'23%'], [0.5,54,'54%'], [1.0,67,'67%'], [1.8,12,'12%']);
+
+   my @data_cpu = ( ['x',        'y_point1', 'y_point2', 'y_point3'],
+                 ['x_point1', 10,         200,        5],
+                 ['x_point2', 100,        400,        1000],
+                 ['x_point3', 1000,       600,        0],
+               );
 
   $graph->add_data({
     'data' => \@data_cpu,
@@ -116,14 +112,9 @@ to using the internal style sheet.
   });
 
 This method allows you to add data to the graph object.  The
-data are expected to be either a list of scalars (in which
-case pairs of elements are taken to be X,Y pairs) or a list
-of array references.  In the latter case, the first two
-elements in each referenced array are taken to be X and Y,
-and the optional third element (if present) is used as the
-text to display for that point for show_data_values and
-rollover_values; otherwise the Y value itself is displayed.
-It can be called several times to add more data sets in.
+data are expected to be either a array of hashes or as a 2D 
+matrix (array of arrays), with the Y-axis as the first arrayref, 
+and the X-axis values in the first element of subsequent arrayrefs.
 
 =head2 clear_data()
 
@@ -153,17 +144,6 @@ created.
 
 =over 4
 
-=item height()
-
-Set the height of the graph box, this is the total height
-of the SVG box created - not the graph it self which auto
-scales to fix the space.
-
-=item width()
-
-Set the width of the graph box, this is the total width
-of the SVG box created - not the graph it self which auto
-scales to fix the space.
 
 =item compress()
 
@@ -173,173 +153,26 @@ Whether or not to compress the content of the SVG file (Compress::Zlib required)
 
 Whether or not to tidy the content of the SVG file (XML::Tidy required).
 
-=item style_sheet()
+=item block_width()
 
-Set the path to an external stylesheet, set to '' if
-you want to revert back to using the default internal version.
+The width of the blocks in px.
 
-The default stylesheet handles up to 12 data sets. All data series over
-the 12th will have no style and be in black. If you have over 12 data
-sets you can assign them all random colors (see the random_color()
-method) or create your own stylesheet and add the additional settings
-for the extra data sets.
+=item block_height()
 
-To create an external stylesheet create a graph using the
-default internal version and copy the stylesheet section to
-an external file and edit from there.
+The height of the blocks in px.
 
-=item random_colors()
+=item gutter()
 
-Use random colors in the internal stylesheet.
+The space between the blocks in px.
 
-=item show_data_values()
+=item y_axis_order()
 
-Show the value of each element of data on the graph (or
-optionally a user-defined label; see add_data).
+ This is order the columns are presented on the y-axis, if the data is in a Array of hashes,
+ this has to be set, however is the data is in an 2D matrix (array of arrays), it will use
+ the order presented in the header array. 
 
-=item show_data_points()
-
-Show a small circle on the graph where the line
-goes from one point to the next.
-
-=item rollover_values()
-
-Shows data values and data points when the mouse is over the point.
-Used in combination with show_data_values and/or show_data_points.
-
-=item data_value_format()
-
-Format specifier to for data values (as per printf).
-
-=item max_x_span()
-
-Maximum span for a line between data points on the X-axis. If this span is
-exceeded, the points are not connected. This is useful for skipping missing data
-sections. If you set this value to something smaller than 0 (e.g. -1), you will
-get an XY scatter plot with no line joining the data points.
-
-=item stacked()
-
-Accumulates each data set. (i.e. Each point increased by
-sum of all previous series at same point). Default is 0,
-set to '1' to show.
-
-=item min_yscale_value()
-
-The point at which the Y axis starts, defaults to '0',
-if set to '' it will default to the minimum data value.
-
-=item max_yscale_value()
-
-The point at which the Y axis ends,
-if set to '' it will default to the maximum data value.
-
-=item yscale_divisions()
-
-This defines the gap between markers on the Y axis,
-default is a 10th of the range, e.g. you will have
-10 markers on the Y axis. NOTE: do not set this too
-low - you are limited to 999 markers, after that the
-graph won't generate.
-
-=item show_x_labels()
-
-Whether to show labels on the X axis or not, defaults
-to 1, set to '0' if you want to turn them off.
-
-=item show_y_labels()
-
-Whether to show labels on the Y axis or not, defaults
-to 1, set to '0' if you want to turn them off.
-
-=item y_label_format()
-
-Format string for presenting the Y axis labels (as per printf).
-
-=item xscale_divisions()
-
-This defines the gap between markers on the X axis.
-Default is the entire range (only start and end axis
-labels).
-
-=item stagger_x_labels()
-
-This puts the labels at alternative levels so if they
-are long field names they will not overlap so easily.
-Default it '0', to turn on set to '1'.
-
-=item rotate_x_labels()
-
-This turns the X axis labels by 90 degrees.
-Default it '0', to turn on set to '1'.
-
-=item min_xscale_value()
-
-This sets the minimum X value. Any data points before this value will not be
-shown.
-
-=item max_xscale_value()
-
-This sets the maximum X value. Any data points after this value will not be
-shown.
-
-=item show_x_title()
-
-Whether to show the title under the X axis labels,
-default is 0, set to '1' to show.
-
-=item x_title()
-
-What the title under X axis should be, e.g. 'Parameter X'.
-
-=item show_y_title()
-
-Whether to show the title under the Y axis labels,
-default is 0, set to '1' to show.
-
-=item y_title()
-
-What the title under Y axis should be, e.g. 'Sales in thousands'.
-
-=item show_graph_title()
-
-Whether to show a title on the graph,
-default is 0, set to '1' to show.
-
-=item graph_title()
-
-What the title on the graph should be.
-
-=item show_graph_subtitle()
-
-Whether to show a subtitle on the graph,
-default is 0, set to '1' to show.
-
-=item graph_subtitle()
-
-What the subtitle on the graph should be.
-
-=item key()
-
-Whether to show a key, defaults to 0, set to
-'1' if you want to show it.
-
-=item key_position()
-
-Where the key should be positioned, defaults to
-'right', set to 'bottom' if you want to move it.
-
-=item x_label_formatter ()
-
-A callback subroutine which will format a label on the x axis.  For example:
-
-    $graph->x_label_formatter( sub { return '$' . $_[0] } );
-
-=item y_label_formatter()
-
-A callback subroutine which will format a label on the y axis.  For example:
-
-    $graph->y_label_formatter( sub { return '$' . $_[0] } );
+ If the data is given in a 2D matrix, and the y_axis_order is set, the y_axis_order will take 
+ prescience.
 
 =back
 
@@ -360,6 +193,7 @@ L<SVG::TT::Graph::Bar>,
 L<SVG::TT::Graph::BarHorizontal>,
 L<SVG::TT::Graph::BarLine>,
 L<SVG::TT::Graph::Pie>,
+L<SVG::TT::Graph::Buuble>,
 L<Compress::Zlib>,
 L<XML::Tidy>
 
@@ -379,48 +213,11 @@ sub _set_defaults
     my %default = (
         'fields' => \@fields,
 
-        'width'  => '500',
-        'height' => '300',
+        'block_width'  => 24,
+        'block_height' => 24,
+        'gutter'       => 1,
 
-        'style_sheet'   => '',
-        'random_colors' => 0,
-
-        'show_data_points' => 1,
-        'show_data_values' => 1,
-        'rollover_values'  => 0,
-
-        'max_x_span' => '',
-
-        'area_fill' => 0,
-
-        'show_y_labels'    => 1,
-        'yscale_divisions' => '',
-        'min_yscale_value' => '0',
-
-        'stacked' => 0,
-
-        'show_x_labels'     => 1,
-        'stagger_x_labels'  => 0,
-        'rotate_x_labels'   => 0,
-        'xscale_divisions'  => '',
-        'x_label_formatter' => sub {return @_},
-        'y_label_formatter' => sub {return @_},
-
-        'show_x_title' => 0,
-        'x_title'      => 'X Field names',
-
-        'show_y_title' => 0,
-        'y_title'      => 'Y Scale',
-
-        'show_graph_title'    => 0,
-        'graph_title'         => 'Graph Title',
-        'show_graph_subtitle' => 0,
-        'graph_subtitle'      => 'Graph Sub Title',
-
-        'key'                  => 0,
-        'key_position'         => 'right',    # bottom or right
-        'y_axis_order'         => [],
-        'include_undef_values' => 0
+        'y_axis_order' => [],
                   );
 
     while ( my ( $key, $value ) = each %default )
@@ -449,7 +246,7 @@ sub add_data
         croak 'There can only be a single piece of data';
     }
 
-    # If there is an order this takes presidence and all data points should have this
+    # If there is an order this takes prescience and all data points should have this
     # however if there are
 
     my %check;
